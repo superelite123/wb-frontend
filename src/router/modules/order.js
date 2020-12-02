6b57d33a482f02c82806fb88986829948e5a23cb
DESKTOP-89IN90F\coolb <topsuperelite@hotmail.com>
2020-12-02 20:51:37 +0800

    first commit

---
+++ src/router/modules/order.js
@@ -0,0 +1,31 @@
+/** When your routing table is too long, you can split it into small modules**/
+import Layout from '@/layout';
+
+const orderRoutes = {
+  path: '/orders',
+  component: Layout,
+  redirect: '/orders/form1',
+  name: 'Walnut Order',
+  alwaysShow: true,
+  meta: {
+    title: 'Walnut Order',
+    icon: 'el-icon-notebook-2',
+    permissions: ['view menu customer relation'],
+  },
+  children: [
+    {
+      path: 'form',
+      component: () => import('@/views/orders/form1'),
+      name: 'OrderForm',
+      meta: { title: 'WB New PO', icon: 'el-icon-plus', permissions: ['manage customer'] },
+    },
+    {
+      path: 'archive0',
+      component: () => import('@/views/orders/archive0'),
+      name: 'OrderPending',
+      meta: { title: 'WB Pending PO', icon: 'el-icon-folder-opened', permissions: ['manage customer'] },
+    },
+  ],
+};
+
+export default orderRoutes;
